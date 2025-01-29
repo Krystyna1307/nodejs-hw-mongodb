13a4d29b8707226b948bd36daf8ed5017712c292
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-01-29 15:03:32 +0100

    Pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,27 @@
 import ContactCollection from '../db/models/Contact.js';
 
-export const getContacts = () => ContactCollection.find(); //якщо сервіс викидає помилку, вона вилітає з controllers далі 2
+import { calcPaginationData } from '../utils/calcPaginationData.js';
+
+export const getContacts = async ({ page = 1, perPage = 10 }) => {
+  const limit = perPage;
+  const skip = (page - 1) * limit; // рахуємо скільки сторінок треба пропустити
+
+  const data = await ContactCollection.find().skip(skip).limit(limit); //якщо сервіс викидає помилку, вона вилітає з controllers далі 2
+  const totalItems = await ContactCollection.countDocuments(); //загальна кількість (одразу число)
+
+  const paginationData = calcPaginationData({ totalItems, page, perPage });
+
+  return {
+    data,
+    page,
+    perPage,
+    totalItems,
+    ...paginationData,
+    // totalPages: 2,
+    // hasPreviousPage: true,
+    // hasNextPage: false,
+  };
+};
 
 export const getContactById = (id) => ContactCollection.findById(id);
 
