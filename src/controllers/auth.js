362cea5739bf362b50ff552aed0d34535a6b476f
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-02-04 18:01:50 +0100

    register new user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,1 +1,11 @@
-export const registerController = async (req, res) => {};
+import * as authServices from '../services/auth.js';
+
+export const registerController = async (req, res) => {
+  const data = await authServices.register(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data,
+  });
+};
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,1 +1,7 @@
 import UserCollection from '../db/models/User.js';
+
+export const register = async (payload) => {
+  const newUser = await UserCollection.create(payload);
+
+  return newUser;
+};
