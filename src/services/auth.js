3fa2db008f4d78ebebff583353f0c039acd77e12
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-02-05 08:32:40 +0100

    register

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,19 @@
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 import UserCollection from '../db/models/User.js';
 
 export const register = async (payload) => {
-  const newUser = await UserCollection.create(payload);
+  const { email, password } = payload;
+  const user = await UserCollection.findOne({ email });
+  if (user) {
+    throw createHttpError(409, 'Email in use');
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await UserCollection.create({
+    ...payload,
+    password: hashPassword,
+  });
 
   return newUser;
 };
