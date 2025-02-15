56b2cd56b211d02dae4589e231aa9bf16e417c17
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-02-15 20:53:05 +0100

    const api_secret

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -4,7 +4,7 @@ import { getEnvVar } from './getEnvVar.js';
 
 const cloud_name = getEnvVar('CLOUDINARY_CLOUD_NAME');
 const api_key = getEnvVar('CLOUDINARY_API_KEY');
-const api_secret = getEnvVar('yE5Ni15LTesbgwxTvDJ89rYnI1U');
+const api_secret = getEnvVar('CLOUDINARY_API_SECRET');
 
 cloudinary.config({
   cloud_name,
